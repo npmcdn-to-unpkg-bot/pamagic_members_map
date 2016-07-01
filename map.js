50317cc557e6c3a376239a5447960d0c079e4c9a
Patrick McKinney <pnmcartography@gmail.com>
2016-07-01 09:04:25 -0400

    added skeleton code to map.js

---
+++ map.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/****************
+*** Variables ***
+****************/
+
+// viewport
+//var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+//var windowArea = windowWidth * windowHeight;
+// map and controls
+var map;
+var homeCoords = [40.914722, -77.774722];
+var initZoom = 8;
+var zoomHome;
+// layer control
+var basemapGroup;
+//var overlayGroup;
+var layerControl;
+
+
+/*********************
+*** Map & Controls ***
+**********************/
+
+// Map
+map = L.map('map', {
+   center: homeCoords,
+   zoom: initZoom,
+   zoomControl: false   
+});
+
+// Zoom Home Control
+zoomHomeControl = L.Control.zoomHome({
+    position: 'topleft',
+    zoomHomeTitle: 'Full map extent',
+    homeCoordinates: homeCoords,
+    homeZoom: initZoom
+}).addTo(map);
+
+/*** Layer Control ***/
+basemapGroup = {};
+
+//overlayGroup = {};
+
+/* Original
+layerControl = L.control.layers(basemapGroup, overlayGroup, {
+    collapsed: setLayerControlCollapsedValue(windowWidth) 
+}).addTo(map);
+*/
+layerControl = L.control.layers(basemapGroup, null).addTo(map);
