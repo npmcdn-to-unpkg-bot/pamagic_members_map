c7c94125ffc709668eb245bcb4987edd548787d6
Patrick McKinney <pnmcartography@gmail.com>
2016-07-08 11:30:51 -0400

    add feature layer search to geocode widget

--- assets/js/geocode.js
+++ assets/js/geocode.js
@@ -1,5 +1,5 @@
 /*********************************************************************
-* Name: ESRI Leaflet Address Search
+* Name: ESRI Leaflet Address Search & Feature Layer Search
 * Version: 1.1
 * Created by: Patrick McKinney, Cumberland County GIS & ESRI
 * Code sample from ESRI Leaflet Calcite project - https://github.com/Esri/calcite-maps/tree/master/samples/esri-leaflet
@@ -20,18 +20,27 @@ function addressLocator() {
     var addressSearchControl;
     var addressSearchResults;
     
-    /*** Address Search Tool ***/
     addressSearchControl = new L.esri.Geocoding.Controls.Geosearch({
+        providers: [
+            new L.esri.Geocoding.Controls.Geosearch.Providers.FeatureLayer({
+            url: 'http://services2.arcgis.com/1mAVlDOwOjpt8pyU/arcgis/rest/services/PAMAGIC_Members/FeatureServer/0',
+            label: 'PAMAGIC Members',    
+            searchFields: ['FirstName', 'LastName'],
+                formatSuggestion: function(feature){
+                return feature.properties.FirstName + ' ' + feature.properties.LastName;
+            }
+          })
+        ],
         zoomToResult: true,
         collapseAfterResult: false,
         expanded: true,
+        useMapBounds: false,    
         allowMultipleResults: true,
         maxResults: 5,
-        useArcgisWorldGeocoder: true,	
-        placeholder: 'Search for an address',
-        title: 'Address Search',
-        useMapBounds: false
-    }).addTo(map);
+        useArcgisWorldGeocoder: true,
+        placeholder: 'Search for Address or Members',
+        title: 'Address & Members Search'    
+   }).addTo(map);
 
     /*** Layer to hold search results ***/
     addressSearchResults = new L.LayerGroup().addTo(map);
@@ -40,11 +49,12 @@ function addressLocator() {
     addressSearchControl.on('results', function(data) {  
        if (data.results.length > 0) {
            // open pop-up for location
+           map.setView(data.results[0].latlng);
            var popup = L.popup()
            .setLatLng(data.results[0].latlng)
            .setContent(data.results[0].text)
            .openOn(map);
-           map.setView(data.results[0].latlng);
+           //map.setView(data.results[0].latlng);
        }
     });
 }
