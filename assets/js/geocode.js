d46cb8ceb77a26014488ff3f3fd4c39d5e526728
Patrick McKinney <pnmcartography@gmail.com>
2016-07-12 19:28:45 -0400

    first pages commit

---
+++ assets/js/geocode.js
@@ -0,0 +1,59 @@
+/*********************************************************************
+* Name: ESRI Leaflet Address Search & Feature Layer Search
+* Version: 1.1
+* Created by: Patrick McKinney, Cumberland County GIS & ESRI
+* Code sample from ESRI Leaflet Calcite project - https://github.com/Esri/calcite-maps/tree/master/samples/esri-leaflet
+* Notes: for use with ESRI Leaflet Geocoder version 1.0.2
+* requires variable windowArea which is window height * window width
+* requires jQuery
+**********************************************************************/
+
+/*
+Notes:
+1. add in code from Bootleaf module for mobile device fixes
+*/
+
+function addressLocator() {
+    'use strict';
+    
+    /*** Variables ***/
+    var addressSearchControl;
+    var addressSearchResults;
+    
+    addressSearchControl = new L.esri.Geocoding.Controls.Geosearch({
+        providers: [
+            new L.esri.Geocoding.Controls.Geosearch.Providers.FeatureLayer({
+            url: 'http://services2.arcgis.com/1mAVlDOwOjpt8pyU/arcgis/rest/services/PAMAGIC_Members/FeatureServer/0',
+            label: 'PAMAGIC Members',    
+            searchFields: ['FirstName', 'LastName'],
+                formatSuggestion: function(feature){
+                return feature.properties.FirstName + ' ' + feature.properties.LastName;
+            }
+          })
+        ],
+        zoomToResult: true,
+        collapseAfterResult: false,
+        expanded: true,
+        useMapBounds: false,    
+        allowMultipleResults: true,
+        maxResults: 5,
+        useArcgisWorldGeocoder: true,
+        placeholder: 'Search for Address or Members',
+        title: 'Address & Members Search'    
+   }).addTo(map);
+
+    /*** Layer to hold search results ***/
+    addressSearchResults = new L.LayerGroup().addTo(map);
+
+    /*** Address search results event ***/
+    addressSearchControl.on('results', function(data) {  
+       if (data.results.length > 0) {
+           // open pop-up for location
+           map.setView(data.results[0].latlng);
+           var popup = L.popup({maxWidth: setPopupMaxWidth(windowWidth)})
+           .setLatLng(data.results[0].latlng)
+           .setContent(data.results[0].text)
+           .openOn(map);           
+       }
+    });
+}
---
+++ assets/js/map.js
@@ -0,0 +1,87 @@
+"use strict";
+
+/****************
+*** Variables ***
+****************/
+
+// viewport
+var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+var windowArea = windowWidth * windowHeight;
+// Map & Controls
+var map;
+var homeCoords = [40.914722, -77.774722];
+var initZoom = setInitialMapZoom(windowWidth);
+var zoomHomeControl;
+// layer control
+var basemapGroup;
+//var overlayGroup;
+var layerControl;
+// Layers
+var esriGray;
+var esriStreets;
+var pmgMembers;
+
+/****************
+*** Functions ***
+*****************/
+// Change map zoom level based upon viewport width
+function viewportChangeMapZoom(windowWidth) {
+    if (windowWidth < 500) {
+        map.setZoom(6);
+    } else if (windowWidth >= 500 && windowWidth < 1000) {
+        map.setZoom(7);
+    } else {
+       map.setZoom(8);
+    }
+}
+
+/*********************
+*** Map & Controls ***
+**********************/
+
+// Map
+map = L.map('map', {
+   center: homeCoords,
+   zoom: initZoom,
+   zoomControl: false
+});
+
+// Zoom Home Control
+zoomHomeControl = L.Control.zoomHome({
+    position: 'topleft',
+    zoomHomeTitle: 'Full map extent',
+    homeCoordinates: homeCoords,
+    homeZoom: initZoom
+}).addTo(map);
+
+// Layers
+esriGray = L.esri.basemapLayer('Gray').addTo(map);
+esriStreets = L.esri.basemapLayer('Streets');
+
+pmgMembers = L.esri.featureLayer({
+    url: 'http://services2.arcgis.com/1mAVlDOwOjpt8pyU/arcgis/rest/services/PAMAGIC_Members/FeatureServer/0',
+    onEachFeature: function (feature, layer) {
+        var popupContent = L.Util.template('<div class="feat-popup"><h2>{FirstName} {LastName}</h2><hr /><h4>Contact Information</h4><ul><li>Phone Number: {TelephoneN}</li><li>E-mail: {EmailAddre}</li><li>Address: {AddressLin}, {City_1}, {State } {ZipCode}</li></ul></div>', feature.properties);
+		layer.bindPopup(popupContent, {closeOnClick: true, maxHeight: setPopupMaxHeight(windowArea), maxWidth: setPopupMaxWidth(windowWidth)});
+    }
+}).addTo(map);
+
+/*** Layer Control ***/
+basemapGroup = {
+    'Gray Canvas': esriGray,
+    'Streets': esriStreets
+};
+
+layerControl = L.control.layers(basemapGroup, null, {
+    collapsed: selectLayerControlCollapsed(windowWidth)
+}).addTo(map);
+
+// Geocode module
+addressLocator();
+
+// GeoLocate module
+locateControl();
+
+// not working properly
+window.addEventListener('resize', viewportChangeMapZoom, false);
