41f63d0f408f002c202ec758ab49d4d2428d93ec
Patrick McKinney <pmacMaps@users.noreply.github.com>
2016-07-21 16:12:37 -0400

    remove zoom resize event function

--- assets/js/map.js
+++ assets/js/map.js
@@ -22,20 +22,6 @@ var esriGray;
 var esriStreets;
 var pmgMembers;
 
-/****************
-*** Functions ***
-*****************/
-// Change map zoom level based upon viewport width
-function viewportChangeMapZoom(windowWidth) {
-    if (windowWidth < 500) {
-        map.setZoom(6);
-    } else if (windowWidth >= 500 && windowWidth < 1000) {
-        map.setZoom(7);
-    } else {
-       map.setZoom(8);
-    }
-}
-
 /*********************
 *** Map & Controls ***
 **********************/
@@ -82,6 +68,3 @@ addressLocator();
 
 // GeoLocate module
 locateControl();
-
-// not working properly
-window.addEventListener('resize', viewportChangeMapZoom, false);
